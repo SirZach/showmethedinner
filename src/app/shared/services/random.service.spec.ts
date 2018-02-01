5397a739ddf05869a695ee94a3a3ba5a92336704
Zach McGonigal <zach.mcgonigal@gmail.com>
2018-01-31 19:15:23 -0500

    Large refactor of random services
    closes #21 closes #23

--- src/app/shared/services/random.service.spec.ts
+++ src/app/shared/services/random.service.spec.ts
@@ -1,42 +1,27 @@
 
 import { RandomDinnerService } from './random.service';
-import { AuthService } from './auth.service';
-import { FoodService } from './food.service';
 import { Dinner } from '../../models';
 import { TestBed, inject } from '@angular/core/testing';
 
 describe('RandomService', () => {
   let $random: RandomDinnerService;
-  let $food: FoodService;
-  const authStub = {
-    user: {
-      mealsCount: 6
-    }
-  };
-  const foodStub = {
-
-  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        RandomDinnerService,
-        { provide: AuthService, useValue: authStub },
-        { provide: FoodService, useValue: foodStub }
+        RandomDinnerService
       ]
     });
 
     $random = TestBed.get(RandomDinnerService);
-    $food = TestBed.get(FoodService);
   });
 
   afterEach(() => {
     $random.dinners = [];
-    $food.dinners = [];
   });
 
   it('indexInUse', () => {
-    $food.dinners = [
+    const foodDinners = [
       new Dinner({ id: 'a' }),
       new Dinner({ id: 'b' }),
       new Dinner({ id: 'c' })
@@ -45,9 +30,9 @@ describe('RandomService', () => {
       new Dinner({ id: 'a' })
     ];
 
-    expect($random.indexInUse(0)).toBeTruthy();
-    expect($random.indexInUse(1)).toBeFalsy();
-    expect($random.indexInUse(2)).toBeFalsy();
+    expect($random.indexInUse(0, foodDinners)).toBeTruthy();
+    expect($random.indexInUse(1, foodDinners)).toBeFalsy();
+    expect($random.indexInUse(2, foodDinners)).toBeFalsy();
   });
 
   it('can calculate numberOfMeals', () => {
@@ -65,7 +50,7 @@ describe('RandomService', () => {
       new Dinner({ meals: 2 }),
       new Dinner({ meals: 1 })
     ];
-    expect($random.numberOfMealsLeft()).toBe(2);
+    expect($random.numberOfMealsLeft(6)).toBe(2);
   });
 
   it('can clearDinners', () => {
@@ -84,7 +69,7 @@ describe('RandomService', () => {
       new Dinner({ meals: 2 }),
       new Dinner({ meals: 1 })
     ];
-    expect($random.canAddDinner()).toBeTruthy();
+    expect($random.canAddDinner(6)).toBeTruthy();
     $random.dinners = [
       new Dinner({ meals: 1 }),
       new Dinner({ meals: 2 }),
@@ -94,7 +79,7 @@ describe('RandomService', () => {
       new Dinner({ meals: 1 }),
       new Dinner({ meals: 1 })
     ];
-    expect($random.canAddDinner()).toBeFalsy();
+    expect($random.canAddDinner(6)).toBeFalsy();
   });
 
   it('calculates if a specific dinner can be added', () => {
@@ -103,15 +88,58 @@ describe('RandomService', () => {
       new Dinner({ meals: 2 }),
       new Dinner({ meals: 1 })
     ];
-    expect($random.canAdd(new Dinner({ meals: 2 }))).toBeTruthy();
-    expect($random.canAdd(new Dinner({ meals: 1 }))).toBeTruthy();
-    expect($random.canAdd(new Dinner({ meals: 3 }))).toBeFalsy();
+    expect($random.canAdd(new Dinner({ meals: 2 }), 6)).toBeTruthy();
+    expect($random.canAdd(new Dinner({ meals: 1 }), 6)).toBeTruthy();
+    expect($random.canAdd(new Dinner({ meals: 3 }), 6)).toBeFalsy();
+  });
+
+  it('addDinner', () => {
+    const a = new Dinner({ id: '1', meals: 2 });
+    const b = new Dinner({ id: '2', meals: 1 });
+    const c = new Dinner({ id: '3', meals: 1 });
+    const d = new Dinner({ id: '4', meals: 2 });
+    const e = new Dinner({ id: '5', meals: 3 });
+    const f = new Dinner({ id: '6', meals: 2 });
+    const g = new Dinner({ id: '7', meals: 2 });
+
+    const foodDinners = [a, b, c, d];
+
+    // Happy path
+    $random.addDinner(foodDinners, 6);
+    $random.addDinner(foodDinners, 6);
+    $random.addDinner(foodDinners, 6);
+    $random.addDinner(foodDinners, 6);
+    expect($random.dinners.length).toBe(4);
+    $random.clearDinners();
+
+    // Error path
+    expect(() => $random.addDinner([e], 2)).toThrowError(`Can't add any more dinners...`);
+    $random.clearDinners();
+
+    // Make sure it stops adding dinners
+    $random.addDinner([a, d, f, g], 6);
+    $random.addDinner([a, d, f, g], 6);
+    $random.addDinner([a, d, f, g], 6);
+    expect(() => $random.addDinner([a, d, f, g], 6)).toThrowError(`Can't add any more dinners...`);
   });
 
   it('can getRandomIndex', () => {
-    expect($random.getRandomIndex(10)).toBeGreaterThan(-1);
-    expect($random.getRandomIndex(10)).toBeLessThan(10);
-    expect($random.getRandomIndex(1)).toBe(0);
+    const indexTracker = {
+      0: true,
+      1: true,
+      2: true,
+      3: true,
+      4: false,
+      5: false,
+      6: true,
+      7: true
+    };
+    expect($random.getRandomIndex(indexTracker)).toBeGreaterThan(3);
+    expect($random.getRandomIndex(indexTracker)).toBeLessThan(10);
+    indexTracker[4] = true;
+    expect($random.getRandomIndex(indexTracker)).toBe(5);
+    indexTracker[5] = true;
+    expect($random.getRandomIndex(indexTracker)).toBe(-1);
   });
 
   it('should remove a dinner', () => {
@@ -133,5 +161,51 @@ describe('RandomService', () => {
     expect($random.isSubsetSum([3, 2, 4], 6)).toBeTruthy();
     expect($random.isSubsetSum([2, 3, 3], 4)).toBeFalsy();
     expect($random.isSubsetSum([1, 1, 1, 1], 6)).toBeFalsy();
+  });
+
+  it('when generating random dinners, it cannot get in an infinite loop', () => {
+    const foodDinners = [
+      new Dinner({ id: '1', meals: 3 }),
+      new Dinner({ id: '2', meals: 3 })
+    ];
+
+    $random.generateRandomDinners(foodDinners, 6);
+    expect(true).toBeTruthy();
+
+    $random.clearDinners();
+
+    expect(() => $random.generateRandomDinners(foodDinners, 5))
+      .toThrowError(`Can't add any more dinners...`);
+  });
+
+  it('anyIndicesLeft', () => {
+    const hasIndicesLeft = {
+      0: false,
+      1: true,
+      2: false,
+      3: true
+    };
+    const noIndicesLeft = {
+      0: true,
+      1: true,
+      2: true
+    };
+    expect($random.anyIndicesLeft(hasIndicesLeft)).toBeTruthy();
+    expect($random.anyIndicesLeft(noIndicesLeft)).toBeFalsy();
+  });
+
+  it('canReplaceDinner', () => {
+    const a = new Dinner({ id: '1', meals: 2 });
+    const b = new Dinner({ id: '2', meals: 1 });
+    const c = new Dinner({ id: '3', meals: 1 });
+    const d = new Dinner({ id: '4', meals: 2 });
+    const e = new Dinner({ id: '5', meals: 3 });
+    const f = new Dinner({ id: '6', meals: 2 });
+    const g = new Dinner({ id: '7', meals: 2 });
+
+    $random.dinners = [a, e, b];
+    $random.replaceDinner(c, [a, b, c, d, e, f, g], 6);
+
+    expect($random.numberOfMealsLeft(6)).toBe(0);
   });
 });
